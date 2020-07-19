8d1a70ce0a4d0660ade92a66719ed8a6676cc1c2
danqinh <59631235+danqinh@users.noreply.github.com>
2020-07-19 11:56:27 -0700

    Use Material UI to add styling to the app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/main';
 
-const testPhrase = "Hello World! This is a test";
-
 ReactDOM.render(<Main />, document.getElementById('app'));
+
+
