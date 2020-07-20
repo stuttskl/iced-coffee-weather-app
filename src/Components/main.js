df83504fe0f98446c9cf83447bd7c41aa5b67421
tdraxler <twdrax@gmail.com>
2020-07-19 17:23:44 -0700

    Add some styling with hooks. Fix the build script so that when webpack is watching for changes, it checks within subdirectories. Also fix some formatting to keep things more consistent

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { mainStyles } from '../styles/styles';
+
+/* One way of styling with Material UI is to use hooks.
+   However, these only work inside functions, so we can't have this with
+   the Main class below.*/
+function HeaderHook() {
+    const classes = mainStyles();
+    return (
+        <div className={classes.mainHeader}>
+            <Typography variant="h3" gutterBottom>
+                Welcome to Weather Roasters
+            </Typography>
+        </div>
+    );
+}
 
 class Main extends React.Component {
     constructor(props) {
@@ -15,10 +31,10 @@ class Main extends React.Component {
     render() {
         return (
             <div>
-                <h1>Welcome to Weather Roasted</h1>
+                <HeaderHook />
                 <Button variant="contained" color="primary">
                     Hello World
-                 </Button>
+                </Button>
             </div>
         )
     }
