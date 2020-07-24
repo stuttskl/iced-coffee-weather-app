1c2b83595d2ca556d4fb2fa8c89ff4909740339c
mswarts <meg.swarts@gmail.com>
2020-07-23 21:31:36 -0700

    second commit

--- APIcall.js
+++ APIcall.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 var app = express();
 
 app.get("/results", function(req,res) {
-    axios("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=34e47a9c09f7eed541ec45ab96dda7e8", function(error, response, body) {
+    axios("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid=<enter your API key here>", function(error, response, body) {
         if(!error && response.statusCode == 200){
             var parsedData = JSON.parse(body);
             res.send(results["temp"]);
