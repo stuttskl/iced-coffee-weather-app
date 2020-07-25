bd55d22235d58630f7b9284bee3f5c46c40a3a17
mswarts <meg.swarts@gmail.com>
2020-07-25 12:55:31 -0700

    cleaned up and fixed indentation

--- routes/APIcall.js
+++ routes/APIcall.js
@@ -4,20 +4,14 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get("/", function(req,res) {
-    //axios("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+ process.env.WEATHER_API_KEY, function(error, response, body) {
     axios.get("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+ process.env.WEATHER_API_KEY)
       .then(function(response) {
           res.send(response.data);
       })
+      //catch error
       .catch(function(error) {
           res.sendStatus(400);
       })
-      
-    //if(!error && response.statusCode == 200){
-            //var parsedData = JSON.parse(body);
-           // res.send(results["temp"]);
-      //  }
-   // });
-    });
-//});
+});
+
 module.exports = router;
