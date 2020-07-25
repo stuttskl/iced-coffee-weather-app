6fc947840d73b7478e2b047784db4b9fcf5d9088
Cortesn <59752457+Cortesn@users.noreply.github.com>
2020-07-25 10:59:44 -0700

    Added 24hrs of data

--- src/Components/Graphs/Hourgraph.js
+++ src/Components/Graphs/Hourgraph.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip,
   } from 'recharts';
 
 const data = [
@@ -40,6 +40,70 @@ const data = [
         "time": "4:00PM",
         "temperature": 76
     },
+    {
+        "time": "5:00PM",
+        "temperature": 74
+    },
+    {
+        "time": "6:00PM",
+        "temperature": 72
+    },
+    {
+        "time": "7:00PM",
+        "temperature": 67
+    },
+    {
+        "time": "8:00PM",
+        "temperature": 65
+    },
+    {
+        "time": "9:00PM",
+        "temperature": 63
+    },
+    {
+        "time": "10:00PM",
+        "temperature": 62
+    },
+    {
+        "time": "11:00PM",
+        "temperature": 60
+    },
+    {
+        "time": "12:00PM",
+        "temperature": 56
+    },
+    {
+        "time": "1:00AM",
+        "temperature": 55
+    },
+    {
+        "time": "2:00AM",
+        "temperature": 55
+    },
+    {
+        "time": "3:00AM",
+        "temperature": 57
+    },
+    {
+        "time": "4:00AM",
+        "temperature": 59
+    },
+    {
+        "time": "5:00AM",
+        "temperature": 60
+    },
+    {
+        "time": "6:00AM",
+        "temperature": 62
+    },
+    {
+        "time": "7:00AM",
+        "temperature": 66
+    },
+    {
+        "time": "8:00AM",
+        "temperature": 69
+    },
 ];
 
 export class HourGraph extends React.Component {
@@ -48,16 +112,13 @@ export class HourGraph extends React.Component {
   }
 
   render() {
-    // TODO
-    // Implement main weather component: temperature, humidity, wind speed, UV index
-    // https://app.asana.com/0/1182915487899489/1183169567980045
     return (
         <LineChart width={730} height={250} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="time" />
-        <YAxis label={{ value: '°F', angle: -90, position: 'insideLeft' }} />
-        <Tooltip />
-        <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="time" />
+            <YAxis label={{ value: '°F', angle: -90, position: 'insideLeft' }} />
+            <Tooltip />
+            <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
         </LineChart>
     );
   }
