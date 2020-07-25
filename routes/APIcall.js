dd5991986ff65949eae8681dcd9297f53c9c41d5
mswarts <meg.swarts@gmail.com>
2020-07-25 12:48:54 -0700

    API key is hidden and route was successfully added for the API call

--- routes/APIcall.js
+++ routes/APIcall.js
@@ -1,26 +1,23 @@
 var express = require('express');
-var router = express.Router();
+const router = express.Router();
 
 const axios = require('axios');
 
 router.get("/", function(req,res) {
     //axios("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+ process.env.WEATHER_API_KEY, function(error, response, body) {
-    axios("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+ process.env.WEATHER_API_KEY)
+    axios.get("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+ process.env.WEATHER_API_KEY)
       .then(function(response) {
-          console.log(response);
+          res.send(response.data);
       })
       .catch(function(error) {
-          console.log(error);
+          res.sendStatus(400);
       })
-      .then(function() {
-          console.log("always executes"); 
       
     //if(!error && response.statusCode == 200){
             //var parsedData = JSON.parse(body);
            // res.send(results["temp"]);
       //  }
    // });
-});
-
+    });
+//});
 module.exports = router;
-});
