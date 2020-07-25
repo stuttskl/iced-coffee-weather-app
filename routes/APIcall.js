f34d4786b7d0816de58b9437e5bb70c84ab10497
mswarts <meg.swarts@gmail.com>
2020-07-25 11:58:04 -0700

    fixed axios error catching, added dotenv config

--- routes/APIcall.js
+++ routes/APIcall.js
@@ -4,12 +4,23 @@ var router = express.Router();
 const axios = require('axios');
 
 router.get("/", function(req,res) {
-    axios("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+ process.env.WEATHER_API_KEY, function(error, response, body) {
-        if(!error && response.statusCode == 200){
-            var parsedData = JSON.parse(body);
-            res.send(results["temp"]);
-        }
-    });
+    //axios("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+ process.env.WEATHER_API_KEY, function(error, response, body) {
+    axios("https://api.openweathermap.org/data/2.5/weather?q=Seattle&appid="+ process.env.WEATHER_API_KEY)
+      .then(function(response) {
+          console.log(response);
+      })
+      .catch(function(error) {
+          console.log(error);
+      })
+      .then(function() {
+          console.log("always executes"); 
+      
+    //if(!error && response.statusCode == 200){
+            //var parsedData = JSON.parse(body);
+           // res.send(results["temp"]);
+      //  }
+   // });
 });
 
 module.exports = router;
+});
