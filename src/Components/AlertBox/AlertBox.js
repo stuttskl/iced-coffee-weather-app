4a84759361505af402e62d52e25c10639455a012
Katie <25576005+stuttskl@users.noreply.github.com>
2020-08-07 14:49:50 -0700

    added default alert if no weather data is available

--- src/Components/AlertBox/AlertBox.js
+++ src/Components/AlertBox/AlertBox.js
@@ -1,12 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './AlertBox.css';
-import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import { CardContent } from '@material-ui/core';
 
 export const AlertBox = props => {
-  // const currentTemp = useState(((temperature - 273.15) * 1.8 + 32).toFixed(2));
-  // const currentTemp = useState(0);
   const currentTemp = ((props.temperature - 273.15) * 1.8 + 32).toFixed(2);
   let alert = ''
     if(currentTemp > 96) {
@@ -21,15 +18,18 @@ export const AlertBox = props => {
     else if(currentTemp >= 42 && currentTemp <= 59) {
       alert = "It might be a little chilly outside. You may want to grab a sweater!";
     }
-    else {
+    else if (currentTemp >= 31 && currentTemp <= 4159){
       alert = "It's pretty cold outside. I'd stay in if you can.";
+    } else {
+      alert = "I'm sorry, cannot retrieve weather tip information for some reason."
     }
   
   return (
-    <div>
-      <h1>Weather Tips!</h1>
-      <p>temp is: {currentTemp}</p>
-      <p>{alert}</p>
-    </div>
+    <Card>
+      <CardContent>
+        <h1>Weather Tips!</h1>
+        <p>{alert}</p>
+      </CardContent>
+    </Card>
   )
 };
