18cf9ae9c080105cd4c2d359e1c034f6edf822c1
Cortesn <59752457+Cortesn@users.noreply.github.com>
2020-08-13 19:50:16 -0700

    fixed scale

--- src/Components/Graphs/Forecastgraph.js
+++ src/Components/Graphs/Forecastgraph.js
@@ -6,6 +6,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import {
     BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar,
   } from 'recharts';
+import { changeTempUnits } from '../tempUnitChange';
 
 function Alert(props) {
 return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -29,6 +30,15 @@ export class ForecastGraph extends React.Component {
       </React.Fragment>;
     } 
     else {
+      const units = this.props.units;
+      const formattedData = this.props.dailyData.map(function(temp){
+        return {
+          "Day": temp.Day,
+          "High": changeTempUnits(units, temp.High),
+          "Low": changeTempUnits(units, temp.Low)
+        }
+      });
+
       toRender = <React.Fragment>
         <div className="graph">
             <Grid container spacing={2} justify="center">
@@ -38,14 +48,17 @@ export class ForecastGraph extends React.Component {
                     </Typography>
                 </Grid>
                 <Grid item xs={12} >
-                    <BarChart width={730} height={250} data={this.props.dailyData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <BarChart width={730} height={250} data={formattedData} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
                     <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="Day" />
-                        <YAxis label={{ value: '°F', angle: -90, position: 'insideLeft' }} />
+                        <YAxis label={{ value: '°'+ this.props.units, angle: -90, position: 'insideLeft' }} 
+                        type="number" 
+                        domain={[dataMin => (Math.ceil((dataMin-30)/10) * 10) , dataMax => (Math.ceil((dataMax + 20) / 10)* 10)]} 
+                        />
                         <Tooltip />
                         <Legend />
-                        <Bar dataKey="High" fill="#fcb103" />
                         <Bar dataKey="Low" fill="#03adfc" />
+                        <Bar dataKey="High" fill="#fcb103" />
                     </BarChart>
                 </Grid>
             </Grid>
